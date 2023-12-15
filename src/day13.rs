bfe7cd0b2fb0db94a151ca394343a5543bd3d053
1mickele <1mickele@gmail.com>
2023-12-15 21:56:04 +0100

    day 14

--- src/day13.rs
+++ src/day13.rs
@@ -3,16 +3,15 @@ use std::fs;
 use itertools::Itertools;
 
 fn main() {
-    let mut x = -1;
     let raw_string = fs::read_to_string("./inputs/input13.txt").expect("Unable to read file");
     let (s1,s2) : (isize, isize) = raw_string.split("\n\n")
         .map(|q| {
-            x+= 1;
             let w = q.chars().position(|c| c == '\n').unwrap() as isize;
             let h = (q.len() as isize + 1) / (w + 1) ;
             let q = q.as_bytes();
 
-            let refl = (0..w-1).map(|s| {
+            let refl = 
+            (0..w-1).map(|s| {
                     let d = (w-s-2).min(s) + 1;
                     (s + 1, h * d - (0..h).map(|i| {
                         (0..d).map(|j| {
@@ -22,7 +21,7 @@ fn main() {
                     }).sum::<usize>() as isize)                
                 })
                 .filter(|(_, e)| *e == 0 || *e == 1)
-                .chain(
+            .chain(
                 (0..h-1).map(|s| {
                     let d = (h-s-2).min(s) + 1;
                     (100 * (s + 1), d * w - (0..w).map(|j| {
@@ -33,7 +32,7 @@ fn main() {
                     }).sum::<usize>() as isize)                
                 })
                 .filter(|(_, e)| *e == 0 || *e == 1)
-                ).collect_vec();            
+            ).collect_vec();            
 
             if refl[0].1 == 0 { return (refl[0].0, refl[1].0); } 
             else { return (refl[1].0, refl[0].0); }
---
+++ src/day14.rs
@@ -0,0 +1,101 @@
+use std::{fs, rc::Rc, cell::RefCell};
+
+use itertools::Itertools;
+
+fn tilt(table_view: &Vec<Rc<RefCell<char>>>, side_len: usize) -> &Vec<Rc<RefCell<char>>> {
+    for j in 0..side_len {
+        let mut i: usize = 0;
+        let mut i0: usize = 0;
+        let mut count: usize = 0;
+        loop {
+            while i < side_len && *table_view[side_len * i + j].borrow() != '#' {
+                if *table_view[side_len * i + j].borrow() == 'O' { 
+                    *table_view[side_len * i + j].borrow_mut() = '.';
+                    count += 1; 
+                }
+                i += 1;
+            }
+
+            for k in 0..count 
+                { *table_view[side_len * (i0 + k) + j].borrow_mut() = 'O'; }
+            count = 0;
+            i += 1;
+
+            if i >= side_len { break; }
+            i0 = i;
+        }        
+    }
+    
+    table_view
+}
+
+fn gen_view(table: &Vec<Rc<RefCell<char>>>, 
+    view: &mut Vec<Rc<RefCell<char>>>,
+    side_len: usize,
+    map: fn(usize, usize, usize) -> usize
+) 
+{
+    for i in 0..side_len {
+        for j in 0..side_len {
+            view[side_len * i + j] 
+                = table[map(i, j, side_len)].clone();
+        }
+    }
+}
+
+fn cycle(table: &Vec<Rc<RefCell<char>>>, side_len: usize) {
+    let mut view = table.clone();
+    let dirs = [
+        |i:usize, j:usize, l:usize| { i * l + j },
+        |i, j, l| { j * l + i },
+        |i, j, l| { l * (l - 1 - i) + j },
+        |i, j, l| { l * j + (l - 1 - i) }
+    ];
+    {
+        for map in dirs {
+            gen_view(&table, &mut view, side_len, map);
+            tilt(&view, side_len);
+        }
+    }
+
+}
+
+fn load(table: &Vec<Rc<RefCell<char>>>, side_len: usize) -> usize {
+    let mut s1 = 0;
+    for j in 0..side_len {
+        for i in 0..side_len {
+            s1 += if *table[i * side_len + j].borrow() == 'O' {side_len - i} else { 0 };
+        }
+    }
+    s1
+}
+
+fn main() {
+    let side_len = 10; 
+    let raw_string = fs::read_to_string("./inputs/test.txt")
+        .expect("Unable to read file");
+
+    let platform : Vec<Rc<RefCell<char>>> = raw_string.chars().filter(|c| *c != '\n')
+        .map(|c| Rc::new(RefCell::new(c)))
+        .collect_vec();
+    {
+        for _ in 0..500 {
+            cycle(&platform, side_len);
+        }
+        let mut period: usize = 0;
+        let start = platform.iter()
+            .map(|c| Rc::new(RefCell::new(*c.borrow())))
+            .collect_vec();
+        loop {
+            cycle(&start, side_len);
+            period += 1;
+            if platform == start {
+                break;
+            }
+        }
+        for _ in 0..((1000000000 - 500) % period) {
+            cycle(&platform, side_len);
+        }
+        println!("{}", load(&platform, side_len));
+    }
+}
